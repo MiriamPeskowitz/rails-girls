88aaa43508561caceff36072fa802359f3004aaa
MiriamPeskowitz <miriam5000@gmail.com>
2019-02-20 13:32:31 -0500

    add tests

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
+
+
 RSpec.describe Idea, type: :model do
+  describe 'associations' do
+    it{ is_expected.to have_many(:comments)}
+  end 
+
+  describe "validations" do
+    it{ is_expected.to validate_presence_of :name }
+  end 
+
   it "has a name" do
     # skip
     idea = Idea.create!(name: "My amazing Idea name")
@@ -8,7 +18,9 @@ RSpec.describe Idea, type: :model do
     second_idea = Idea.create!(name: "second amazing idea")
     expect(second_idea.name).to eq("second amazing idea")
   end 
-  it 'has a description'
+  it 'has a description' #this makes the test pending 
 end
+
+
 
 
